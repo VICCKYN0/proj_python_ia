748914da06d613599c430a72d152162516276b77
VITÓRIA GABRIELLE <v1cckyn0@gmail.com>
2024-08-28 18:55:47 -0300

    laço de repetição

---
+++ aula 03/para2.py
@@ -0,0 +1,20 @@
+alunos = [
+    "Rafael",
+    "Vitoria",
+    "Christian",
+    "Matheus",
+    "Richard",
+    "Kaio",
+    "Nicolas",
+    "Maúricio",
+    "Lucas",
+    "Gustavo",
+    "William"
+]
+alunos.sort()
+alunos.append("Nobru")
+alunos.reverse()
+y = 1
+for x in alunos:
+    print (y,"-",x)
+    y = y + 1
