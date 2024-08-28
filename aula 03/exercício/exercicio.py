5796d0227739061504390c6f985e290d99c47391
VITÓRIA GABRIELLE <v1cckyn0@gmail.com>
2024-08-28 20:08:16 -0300

    exercio

---
+++ aula 03/exercício/exercicio.py
@@ -0,0 +1,17 @@
+frutas = [
+    "Abacaxi",
+    "Maçã",
+    "Melancia",
+    "Morango",
+    "Laranja",
+    "Couve",
+    "Uva",
+    "Tomate",
+    "Alface",
+    "Mamão"
+]
+frutas.sort()
+y = 1
+for x in frutas:
+    print (y,"-",x)
+    y = y + 1
